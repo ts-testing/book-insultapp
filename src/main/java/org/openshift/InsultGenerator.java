7806f06e5b0872598556ef50d4fbf8f8536db544
ts-testing <45851426+ts-testing@users.noreply.github.com>
2019-01-18 14:34:06 +0100

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -27,7 +27,7 @@ public class InsultGenerator {
 					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 						article = "a";
 					}
-					theInsult =  String.format("Thou art %s %s %s %s!", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+					theInsult =  String.format("123 Thou art %s %s %s %s!", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
 				}
 				rs.close();
 				connection.close();
